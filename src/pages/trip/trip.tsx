82971b203629172299cbb83ccce7117991008a0e
unknown <jiaegong@ewhain.net>
2022-07-05 14:36:43 +0900

    Fix: Fix tag error

---
+++ src/pages/trip/trip.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Controller from '../../components/controller/controller';
+
+function Trip() {
+  return (
+    <>
+      <video src={`https://www.youtube.com/watch?v=LPgZ4lKfBPw`} autoPlay controls />
+      <Controller />
+    </>
+  );
+}
+
+export default Trip;
